f985dec74deb0b0621725465fe45c5c2598cb3ba
PhysicistProgs <gluhov_1999@inbox.ru>
2021-10-15 16:04:15 +0700

    Создал модель User
    Модель наследуется от AbstractBaseUser для поддержки аутентификации на
    основе email вместо username.
    Переписанный класс UserManager также нужен для поддержки этого
    функционала.

---
+++ api_app/models.py
@@ -0,0 +1,58 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+from django.utils.translation import ugettext as _
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    MALE = 'M'
+    FEMALE = 'F'
+    SEX_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female')
+    )
+
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=30)
+    last_name = models.CharField(_('last name'), max_length=30)
+    sex = models.CharField(_('sex'), choices=SEX_CHOICES, max_length=30)
+    avatar = models.ImageField()
+    is_active = models.BooleanField(_('active'), default=True)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
